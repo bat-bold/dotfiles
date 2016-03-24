b7a13dca82aec2de6f12a01fcacff2ac90a4cbfa
Batbold Gansukh <batbold@Batbolds-iMac.local>
2016-03-25 01:13:40 +0800

    Add all util's manual paths to MANPATH

--- fishshell/config.fish
+++ fishshell/config.fish
@@ -4,6 +4,23 @@ if status --is-interactive
   if test (uname) = "Darwin"
     set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH 
     set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+    set MANPATH /usr/local/opt/wget/share/man $MANPATH
+    set MANPATH /usr/local/opt/binutils/share/man $MANPATH
+    set MANPATH /usr/local/opt/diffutils/share/man $MANPATH
+    set MANPATH /usr/local/opt/ed/share/man $MANPATH
+    set MANPATH /usr/local/findutils/share/man $MANPATH
+    set MANPATH /usr/local/gawk/share/man $MANPATH
+    set MANPATH /usr/local/gnu-indent/share/man $MANPATH
+    set MANPATH /usr/local/gnu-sed/share/man $MANPATH
+    set MANPATH /usr/local/gnu-tar/share/man $MANPATH
+    set MANPATH /usr/local/gnu-which/share/man $MANPATH
+    set MANPATH /usr/local/gnutls/share/man $MANPATH
+    set MANPATH /usr/local/grep/share/man $MANPATH
+    set MANPATH /usr/local/gzip/share/man $MANPATH
+    set MANPATH /usr/local/screen/share/man $MANPATH
+    set MANPATH /usr/local/watch/share/man $MANPATH
+    set MANPATH /usr/local/wdiff/share/man $MANPATH
+    set MANPATH /usr/local/wget/share/man $MANPATH
   end
 end
 
