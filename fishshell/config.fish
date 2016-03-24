5ce28541505758ad4266a471664b5457a32794d2
Batbold Gansukh <batbold@Batbolds-iMac.local>
2016-03-25 03:53:19 +0800

    Fix manual path

--- fishshell/config.fish
+++ fishshell/config.fish
@@ -8,19 +8,19 @@ if status --is-interactive
     set MANPATH /usr/local/opt/binutils/share/man $MANPATH
     set MANPATH /usr/local/opt/diffutils/share/man $MANPATH
     set MANPATH /usr/local/opt/ed/share/man $MANPATH
-    set MANPATH /usr/local/findutils/share/man $MANPATH
-    set MANPATH /usr/local/gawk/share/man $MANPATH
-    set MANPATH /usr/local/gnu-indent/share/man $MANPATH
-    set MANPATH /usr/local/gnu-sed/share/man $MANPATH
-    set MANPATH /usr/local/gnu-tar/share/man $MANPATH
-    set MANPATH /usr/local/gnu-which/share/man $MANPATH
-    set MANPATH /usr/local/gnutls/share/man $MANPATH
-    set MANPATH /usr/local/grep/share/man $MANPATH
-    set MANPATH /usr/local/gzip/share/man $MANPATH
-    set MANPATH /usr/local/screen/share/man $MANPATH
-    set MANPATH /usr/local/watch/share/man $MANPATH
-    set MANPATH /usr/local/wdiff/share/man $MANPATH
-    set MANPATH /usr/local/wget/share/man $MANPATH
+    set MANPATH /usr/local/opt/findutils/share/man $MANPATH
+    set MANPATH /usr/local/opt/gawk/share/man $MANPATH
+    set MANPATH /usr/local/opt/gnu-indent/share/man $MANPATH
+    set MANPATH /usr/local/opt/gnu-sed/share/man $MANPATH
+    set MANPATH /usr/local/opt/gnu-tar/share/man $MANPATH
+    set MANPATH /usr/local/opt/gnu-which/share/man $MANPATH
+    set MANPATH /usr/local/opt/gnutls/share/man $MANPATH
+    set MANPATH /usr/local/opt/grep/share/man $MANPATH
+    set MANPATH /usr/local/opt/gzip/share/man $MANPATH
+    set MANPATH /usr/local/opt/screen/share/man $MANPATH
+    set MANPATH /usr/local/opt/watch/share/man $MANPATH
+    set MANPATH /usr/local/opt/wdiff/share/man $MANPATH
+    set MANPATH /usr/local/opt/wget/share/man $MANPATH
   end
 end
 
