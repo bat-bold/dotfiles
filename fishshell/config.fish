4e3eb6732e9497aa309ec29ff14f1432ea66f05c
Batbold Gansukh <batbold@Batbolds-iMac.local>
2016-09-22 07:52:15 +0900

    postgres.app path config

--- fishshell/config.fish
+++ fishshell/config.fish
@@ -1,7 +1,10 @@
 fish_vi_key_bindings
 
 if status --is-interactive 
-  if test (uname) = "Darwin"
+  if test (uname) = "Darwin" # only on when running in mac osx
+    # PATH config for Postgres.app from https://postgresapp.com
+    set PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
+    # coreutils manpath config
     set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH 
     set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
     set MANPATH /usr/local/opt/wget/share/man $MANPATH
