8e9a8496a1a377cbdfb9c6d9979f97bdd2fa6470
Batbold Gansukh <batbold@Batbolds-iMac.local>
2016-11-08 05:48:31 +0800

    wildcard MANPATH

--- fishshell/config.fish
+++ fishshell/config.fish
@@ -6,25 +6,8 @@ if status --is-interactive
     set PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
     # coreutils manpath config
     set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH 
+    set MANPATH /usr/local/opt/*/share/man $MANPATH
     set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
-    set MANPATH /usr/local/opt/wget/share/man $MANPATH
-    set MANPATH /usr/local/opt/binutils/share/man $MANPATH
-    set MANPATH /usr/local/opt/diffutils/share/man $MANPATH
-    set MANPATH /usr/local/opt/ed/share/man $MANPATH
-    set MANPATH /usr/local/opt/findutils/share/man $MANPATH
-    set MANPATH /usr/local/opt/gawk/share/man $MANPATH
-    set MANPATH /usr/local/opt/gnu-indent/share/man $MANPATH
-    set MANPATH /usr/local/opt/gnu-sed/share/man $MANPATH
-    set MANPATH /usr/local/opt/gnu-tar/share/man $MANPATH
-    set MANPATH /usr/local/opt/gnu-which/share/man $MANPATH
-    set MANPATH /usr/local/opt/gnutls/share/man $MANPATH
-    set MANPATH /usr/local/opt/grep/share/man $MANPATH
-    set MANPATH /usr/local/opt/gzip/share/man $MANPATH
-    set MANPATH /usr/local/opt/screen/share/man $MANPATH
-    set MANPATH /usr/local/opt/watch/share/man $MANPATH
-    set MANPATH /usr/local/opt/wdiff/share/man $MANPATH
-    set MANPATH /usr/local/opt/wget/share/man $MANPATH
-    set MANPATH /usr/local/opt/fswatch/share/man $MANPATH
   end
 end
 
