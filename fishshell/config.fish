3c5c1b957cabb4b7e04b2caea7baa3f2589ddc73
Batbold Gansukh <batbold@Batbolds-iMac.local>
2019-04-28 06:36:39 +0800

    update

--- fishshell/config.fish
+++ fishshell/config.fish
@@ -3,6 +3,9 @@ fish_vi_key_bindings insert
 
 set -g -x SBT_OPTS "-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Xss2M"
 
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+
 if status --is-interactive 
   if test (uname) = "Darwin" # only on when running in mac osx
     # PATH config for Postgres.app from https://postgresapp.com
@@ -11,5 +14,28 @@ if status --is-interactive
     set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH 
     set MANPATH /usr/local/opt/*/share/man $MANPATH
     set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+
+    #make gnu utils available
+    if test -d /usr/local/opt/ed/libexec/gnubin
+      set PATH /usr/local/opt/ed/libexec/gnubin $PATH
+    end
+    if test -d /usr/local/opt/findutils/libexec/gnubin
+      set PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+    end
+    if test -d /usr/local/opt/gnu-indent/libexec/gnubin
+      set PATH /usr/local/opt/gnu-indent/libexec/gnubin $PATH
+    end
+    if test -d /usr/local/opt/gnu-sed/libexec/gnubin
+      set PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+    end
+    if test -d /usr/local/opt/gnu-tar/libexec/gnubin
+      set PATH /usr/local/opt/gnu-tar/libexec/gnubin $PATH
+    end
+    if test -d /usr/local/opt/gnu-which/libexec/gnubin
+      set PATH /usr/local/opt/gnu-which/libexec/gnubin $PATH
+    end
+    if test -d /usr/local/opt/grep/libexec/gnubin
+      set PATH /usr/local/opt/grep/libexec/gnubin $PATH
+    end
   end
 end
