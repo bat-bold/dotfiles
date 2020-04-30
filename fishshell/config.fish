fa11eaeb0fd6432964d7198ccd264484b13fce9f
Batbold Gansukh <batbold1987227@yahoo.com>
2020-05-01 00:24:14 +0800

    remove UseConcMarkSweepGC from SBT

--- fishshell/config.fish
+++ fishshell/config.fish
@@ -1,7 +1,7 @@
 
 fish_vi_key_bindings insert
 
-set -g -x SBT_OPTS "-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Xss2M"
+set -g -x SBT_OPTS "-Xmx2G -XX:+CMSClassUnloadingEnabled -Xss2M"
 
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
@@ -12,6 +12,7 @@ if status --is-interactive
     set PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
     # coreutils manpath config
     set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH 
+    set PATH /usr/local/bin $PATH 
     set MANPATH /usr/local/opt/*/share/man $MANPATH
     set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 
@@ -36,6 +37,10 @@ if status --is-interactive
     end
     if test -d /usr/local/opt/grep/libexec/gnubin
       set PATH /usr/local/opt/grep/libexec/gnubin $PATH
+    end
+
+    if test -d /usr/local/opt/node@12/bin
+      set PATH /usr/local/opt/node@12/bin $PATH
     end
   end
 end
